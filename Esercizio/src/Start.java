62c26cd5d875253439c9015ab2b5513cd38cf66d
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-08 20:56:00 +0100

    Esercizio 2 sul ciclo while

---
+++ Esercizio/src/Start.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i>=0){
+            System.out.println("Please enter a command:");
+            Scanner scanner = new Scanner(System.in);
+            if(scanner.nextLine().equals("stop")){
+                break;
+            }
+
+
+        }
+    }
+}
